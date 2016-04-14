d6011cc9f73d6ff7446f5a6b4d0a0c80b85af937
khomyakeugene <khomyak_eugene@yahoo.com>
2016-04-14 10:48:01 +0300

    JEE Unit 4 Homework

--- src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-import com.company.collections.CollectionEfficiencyDemonstrator;
-
-public class Main {
-    public static void main(String[] args) {
-        new CollectionEfficiencyDemonstrator().demonstrate();
-    }
-}
